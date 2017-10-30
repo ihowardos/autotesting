39f35e8358356f9ea7f2140dc2325e34965b3d3b
Sergey Antonov <asergey96@gmail.com>
2017-10-30 21:35:58 +0300

    init

---
+++ db/migrate/20171030181707_add_user_id_to_posts.rb
@@ -0,0 +1,8 @@
+class AddUserIdToPosts < ActiveRecord::Migration[5.1]
+  def change
+    add_column :posts, :user_id, :integer
+
+    add_index :posts, :user_id
+    add_foreign_key :posts, :users
+  end
+end
